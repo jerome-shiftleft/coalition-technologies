ceb61a10204f95d8f3f65b1eeb99e313649554be
jerome-shiftleft <jerome@theshiftleft.com>
2023-10-16 02:27:55 +0800

    progress

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.dashboard', [
-  'body_id' => 'dashboard-index',
-  'body_class' => 'dashboard',
+    'body_id' => 'dashboard-index',
+    'body_class' => 'dashboard',
 ])
 
 @section('meta-dynamic')
@@ -16,18 +16,71 @@
 
     <div class="content">
 
-      <select name="select_project" id="select-project"
-      class="form-select form-select-sm search-select">
-        <option value="project-1">Project 1</option>
-        <option value="project-2">Project 2</option>
-        <option value="project-3">Project 3</option>
-        <option value="project-4">Project 4</option>
-        <option value="project-5">Project 5</option>
-      </select>
+      <div id="task-manager">
 
-      <div id="task-list">
+        <select name="select_project" id="select-project" class="form-select search-select">
+          <option></option>
+          <option value="project-1">Project 1</option>
+          <option value="project-2">Project 2</option>
+          <option value="project-3">Project 3</option>
+        </select>
 
-      </div><!-- /#task-list -->
+        <div id="task-list">
+
+          <div class="task">
+
+            <div class="task-header">
+              <h3 class="task-title">Task 1</h3>
+              <div class="task-header-actions">
+                <a href="#"><i class="fa-solid fa-angle-down"></i></a>
+                <a href="#"><i class="fa-solid fa-up-down"></i></a>
+                <a href="#"><i class="fa-solid fa-xmark"></i></a>
+              </div><!-- /.task-header-actions -->
+            </div><!-- /.task-header -->
+
+            <div class="task-content">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor provident sed expedita unde, atque, perferendis excepturi sequi asperiores eveniet earum autem sint debitis deserunt iusto quos distinctio deleniti cum sit.</p>
+            </div><!-- /.task-content -->
+
+          </div><!-- /.task -->
+
+          <div class="task">
+
+            <div class="task-header">
+              <h3 class="task-title">Task 2</h3>
+              <div class="task-header-actions">
+                <a href="#"><i class="fa-solid fa-angle-down"></i></a>
+                <a href="#"><i class="fa-solid fa-up-down"></i></a>
+                <a href="#"><i class="fa-solid fa-xmark"></i></a>
+              </div><!-- /.task-header-actions -->
+            </div><!-- /.task-header -->
+
+            <div class="task-content">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor provident sed expedita unde, atque, perferendis excepturi sequi asperiores eveniet earum autem sint debitis deserunt iusto quos distinctio deleniti cum sit.</p>
+            </div><!-- /.task-content -->
+
+          </div><!-- /.task -->
+
+          <div class="task">
+
+            <div class="task-header">
+              <h3 class="task-title">Task 3</h3>
+              <div class="task-header-actions">
+                <a href="#"><i class="fa-solid fa-angle-down"></i></a>
+                <a href="#"><i class="fa-solid fa-up-down"></i></a>
+                <a href="#"><i class="fa-solid fa-xmark"></i></a>
+              </div><!-- /.task-header-actions -->
+            </div><!-- /.task-header -->
+
+            <div class="task-content">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor provident sed expedita unde, atque, perferendis excepturi sequi asperiores eveniet earum autem sint debitis deserunt iusto quos distinctio deleniti cum sit.</p>
+            </div><!-- /.task-content -->
+
+          </div><!-- /.task -->
+
+        </div><!-- /#task-list -->
+
+      </div><!-- /#task-mananager -->
 
     </div><!-- /.content -->
 
