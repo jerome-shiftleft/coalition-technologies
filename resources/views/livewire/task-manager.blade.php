7ec5e1a62c64adf02df0e16ad3e682b516c61e9e
jerome-shiftleft <jerome@theshiftleft.com>
2023-10-22 08:27:41 +0800

    code format cleanup on task manager livewire component

--- resources/views/livewire/task-manager.blade.php
+++ resources/views/livewire/task-manager.blade.php
@@ -22,12 +22,15 @@
         <div class="task">
 
           <div class="task-header">
+            
             <h3 class="task-title">{{ $task->title }}</h3>
+
             <div class="task-header-actions">
               <a href="#" data-bs-toggle="tooltip" data-bs-title="Show/Hide Task Description"><i class="expand-task fa-solid fa-angle-down"></i></a>
               {{-- <a href="#" data-bs-toggle="tooltip" data-bs-title="Reorder Task"><i class="reorder-task fa-solid fa-up-down"></i></a> --}}
               <a href="#" data-bs-toggle="tooltip" data-bs-title="Delete Task"><i class="delete-task fa-solid fa-xmark"></i></a>
             </div><!-- /.task-header-actions -->
+
           </div><!-- /.task-header -->
 
           <div class="task-content">
