bed68eb3ab05bfd84ec65399ad39e66016af2d58
jerome-shiftleft <jerome@theshiftleft.com>
2023-10-22 17:40:52 +0800

    implemented nested components on tasks list

--- resources/views/livewire/task-manager.blade.php
+++ resources/views/livewire/task-manager.blade.php
@@ -1,4 +1,4 @@
-<div id="task-manager" x-data>
+<div id="task-manager">
 
   <div id="select-project-wrap">
 
@@ -22,32 +22,7 @@
 
     <div id="task-list">
       @foreach ($tasks as $task)
-        <div class="task" x-data="{ isTaskExpanded: false }">
-
-          <div class="task-header">
-
-            <h3 class="task-title">{{ $task->title }}</h3>
-
-            <div class="task-header-actions">
-
-              <a href="#" x-on:click="isTaskExpanded = !isTaskExpanded" data-bs-toggle="tooltip"
-                data-bs-title="Show/Hide Task Description">
-                <i class="expand-task fa-solid fa-angle-down"></i>
-              </a>
-
-              <a href="#" data-bs-toggle="tooltip" data-bs-title="Delete Task">
-                <i class="delete-task fa-solid fa-xmark"></i>
-              </a>
-
-            </div><!-- /.task-header-actions -->
-
-          </div><!-- /.task-header -->
-
-          <div class="task-content" x-show="isTaskExpanded" x-transition>
-            {{ $task->description }}
-          </div><!-- /.task-content -->
-
-        </div><!-- /.task -->
+        <livewire:tasks-list :task="$task" :wire:key="$task->id">
       @endforeach
     </div><!-- /#task-list -->
 
