9dcc8f31e94e4b575c237c901314f0ed48231332
jerome-shiftleft <jerome@theshiftleft.com>
2023-10-22 17:49:46 +0800

    database seeder changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,11 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         Project::factory()
-            ->count(10)
+            ->count(5) // Create 5 projects
             ->create()
             ->each(function ($project) {
-                $taskCount = rand(3, 5);
-
+                $taskCount = rand(5, 10); // for each project, create around 5 to 10 tasks
                 Task::factory()
                     ->count($taskCount)
                     ->create(['project_id' => $project->id]);
