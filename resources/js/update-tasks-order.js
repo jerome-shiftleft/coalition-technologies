030ae7678c62f4ff1c7e665e952c819d5e083582
jerome-shiftleft <jerome@theshiftleft.com>
2023-10-24 04:03:59 +0800

    reorder tasks done

--- resources/js/update-tasks-order.js
+++ resources/js/update-tasks-order.js
@@ -1,9 +1,19 @@
 const update_tasks_order = () => {
   console.log('updating tasks order...');
-  $('#task-list >.task').each(function() {
-    var title = $(this).find('.task-title').text();
-    console.log(title);
+
+  var tasks = []
+
+  $('#task-list > .task').each(function(index) {
+    var id = $(this).data('id');
+    var title = $(this).find('.task-title').text();        
+    console.log(`[${index}] - ${id}. ${title}`);
+    tasks.push({id, title});
   });
+
+  tasks.pop();
+  console.log(tasks);
+
+  Livewire.emitTo('task-manager', 'reorderTasks', tasks);
 }
 
 export { update_tasks_order };
