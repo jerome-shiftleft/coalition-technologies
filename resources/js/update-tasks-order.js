658b4a225bc8ed513b0170a546c0305b4f42b5ee
jerome-shiftleft <jerome@theshiftleft.com>
2023-10-24 22:33:55 +0800

    code cleanup

--- resources/js/update-tasks-order.js
+++ resources/js/update-tasks-order.js
@@ -3,10 +3,10 @@ const update_tasks_order = () => {
 
   var tasks = []
 
+  console.log('tasks order: ');
   $('#task-list > .task').each(function (index) {
     var id = $(this).data('id');
-    var title = $(this).find('.task-title').text();
-    console.log('temp raw order: ');
+    var title = $(this).find('.task-title').text();    
     console.log(`[${index}] - ${id}. ${title}`);
     tasks.push({ id, title });
   });  
